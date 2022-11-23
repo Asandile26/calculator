34ca11d99f4bb24ea10bd3b62b95b6332051d5d3
Asandile <asandilelangeni@gmail.com>
2022-11-23 16:18:53 +0200

    first commit

---
+++ js/code.js
@@ -0,0 +1,27 @@
+let numbers = document.querySelectorAll('.number')
+let screen = document.querySelector('#screen')
+numbers.forEach (function(number) {
+    number.addEventListener('click', function(){
+     screen.value += number.textContent
+    })
+
+})
+
+let operators = document.querySelectorAll('.operator')
+operators.forEach (function(operator) {
+    operator.addEventListener('click', function(){
+        screen.value += operator.textContent
+    })
+
+})
+
+let equal = document.querySelector('.equal');
+equal.addEventListener('click', function(){
+    screen.value =eval(screen.value)
+})
+
+let deleteBtn = document.querySelector('.delete');
+deleteBtn.addEventListener('click', function(){
+    screen.value = screen.value.slice('0', '-1')
+})
+
