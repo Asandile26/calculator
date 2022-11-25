549f2620fdae32bf284e1dc531426b3800d3ecdf
Asandile <asandilelangeni@gmail.com>
2022-11-25 16:22:53 +0200

    fourth commit

--- js/code.js
+++ js/code.js
@@ -1,5 +1,8 @@
 let numbers = document.querySelectorAll('.number')
 let screen = document.querySelector('#screen')
+let decimal = document.querySelector('#decimal')
+let allowed = true;
+
 numbers.forEach (function(number) {
     number.addEventListener('click', function(){
      screen.value += number.textContent
@@ -11,6 +14,7 @@ let operators = document.querySelectorAll('.operator')
 operators.forEach (function(operator) {
     operator.addEventListener('click', function(){
         screen.value += operator.textContent
+        allowed = true
     })
 
 })
@@ -18,13 +22,36 @@ operators.forEach (function(operator) {
 let equal = document.querySelector('.equal');
 equal.addEventListener('click', function(){
     screen.value =eval(screen.value)
+    if (screen.value.includes('.')) {
+        allowed = false
+    } else allowed = true
 })
 
 let deleteBtn = document.querySelector('.delete');
 deleteBtn.addEventListener('click', function(){
-    screen.value = screen.value.slice('0', '-1')
+    if (screen.value[screen.value.length -1] == '.') {
+        screen.value = screen.value.slice('0', '-1')
+        allowed = true ;
+    } else {
+        screen.value = screen.value.slice('0', '-1')
+
+    }
 })
 let clearAll = document.getElementById('cal');
 clearAll.addEventListener('click', ()=>{
     screen.value = "";
+    allowed = true
 } )
+
+decimal.addEventListener('click', function(){
+
+    if (allowed) {
+        screen.value += decimal.textContent;
+        allowed = false
+    }
+})
+        
+
+
+
+
